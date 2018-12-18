5c04299db1f23bee7836d73c3334f0306e2615c9
Jitender Kumar <jitender.kumar@irissoftware.com>
2018-12-18 17:49:27 +0530

    Operator in Python

---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+import sys
+import os
+print("5 + 2 = ", 5+2)
+print("5 - 2 = ", 5-2)
+print("5 * 2 = ", 5*2)
+print("5 / 2 = ", 5/2)
+print("5 % 2 = ", 5%2)
+print("5 ** 3 = ", 5**3)
+print("5 // 2 = " , 5//2)
